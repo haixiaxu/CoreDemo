eea54b757d82152cae44b6e194eaa87291906985
xuhaixia <xuhaixia200@163.com>
2019-12-19 17:16:03 +0800

    新增ASP.NET Core 3.0 Api项目

---
+++ Czar.Cms/AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountOwnerServer.Extensions
+{
+    /// <summary>
+    /// 服务扩展方法
+    /// </summary>
+    public static class ServiceExtensions
+    {
+    }
+}
