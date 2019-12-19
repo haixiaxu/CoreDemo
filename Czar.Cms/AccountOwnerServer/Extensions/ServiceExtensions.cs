e5d4e658cb60f9d2edeb630df55ea0af7e9066bd
xuhaixia <xuhaixia200@163.com>
2019-12-19 17:34:56 +0800

    添加服务扩展方法

--- Czar.Cms/AccountOwnerServer/Extensions/ServiceExtensions.cs
+++ Czar.Cms/AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AccountOwnerServer.Extensions
 {
@@ -10,5 +8,27 @@ namespace AccountOwnerServer.Extensions
     /// </summary>
     public static class ServiceExtensions
     {
+        /// <summary>
+        /// 配置Cors,Cors(跨资源共享)是一种机制,授予用户访问其他域中的服务器资源权限               
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+        }
+        /// <summary>
+        /// 配置IIS集成, 不会初始化选项内的任何属性,因为可以使用默认值
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options => { });
+        }
     }
 }
